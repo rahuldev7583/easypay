818a5263b1530607b4cbe8d0efd461c9600a6ff0
Rahul Dev <rahuldev7583@gmail.com>
2024-06-02 00:20:50 +0530

    recoil store added to user-app

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,9 +1,13 @@
+"use client";
 import { Button } from "@repo/ui/components/ui/button";
+import { useBalance } from "@repo/store";
 
 export default function Page(): JSX.Element {
+  const balance = useBalance();
   return (
     <main className="">
       <Button>Button</Button>
+      <p>your balance is {balance}</p>
     </main>
   );
 }
